3503a7845633b5eb8c700685ca99a84810e2dc0b
Geoffrey Routledge <geoff@dgbi.ca>
2025-02-20 19:57:48 +0000

    Promotion of "Reverting 'JIRA-555'"

--- envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr1.js
+++ envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr1.js
@@ -12,11 +12,10 @@ define(['N/record', 'N/search'],
  */
 function(record, search) {
     // GR 20240313-123224
-
     // GR 20240503-132414
 
     // GR 20240723-095848
-
+    // GR 20241003-133854
 
     /**
      * Function to be executed after page is initialized.
@@ -28,9 +27,7 @@ function(record, search) {
      * @since 2015.2
      */
     function pageInit(scriptContext) {
-
-        const s = "20240313-130450"
-
+        const s = "20240313-122646"
         alert("Bonjour tout la monde")
 
     }
